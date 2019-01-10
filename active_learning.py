fixed bug with np.digitize and unceratianty computation

active_learning.py
@@ -94,7 +94,7 @@ def acq_random_emp(n_inc, candidate_score, candidate_list, subset_list, confi=No
 def acq_random_unf(n_inc, candidate_score, candidate_list, subset_list, confi=None):
     num_bins = NUM_BINS
     bins = np.linspace(0, 1, num_bins+1)
-    digitized = np.digitize(candidate_score, bins)
+    digitized = np.digitize(candidate_score, bins[1:-1])
     counter=collections.Counter(digitized.tolist())
     weights = np.array([1.0/counter[digitized[_]] for _ in candidate_list])
     weights[subset_list] = 0
@@ -104,9 +104,7 @@ def acq_random_unf(n_inc, candidate_score, candidate_list, subset_list, confi=No
 
 def acq_active_prb(n_inc, candidate_score, candidate_list, subset_list, confi):
     confi = sigmoid(confi) # 100 * 1
-    uncertainty = confi[:,1] - confi[:, 0]
-    digitized = np.digitize(candidate_score, np.linspace(0, 1, 100)) -1
-    weights = uncertainty[digitized]
+    weights = confi[:,1] - confi[:, 0]
     weights[subset_list] = 0
     p = weights/ weights.sum()
     return np.random.choice(candidate_list, size = n_inc, replace = False, p = p).tolist()
@@ -114,8 +112,6 @@ def acq_active_prb(n_inc, candidate_score, candidate_list, subset_list, confi):
 def acq_active_dtm(n_inc, candidate_score, candidate_list, subset_list, confi):
     import heapq
     confi = sigmoid(confi) # 100 * 1
-    uncertainty = confi[:,1] - confi[:, 0]
-    digitized = np.digitize(candidate_score, np.linspace(0, 1, 100)) -1
-    weights = uncertainty[digitized]
+    weights = confi[:,1] - confi[:, 0]
     weights[subset_list] = 0
     return heapq.nlargest(n_inc, range(len(weights)), weights.__getitem__)

helper.py
@@ -20,7 +20,7 @@ def EceEval(p, y, num_bins):
     bins = np.linspace(0, 1, num_bins+1)
     
     confidence = np.max(p, axis=1)
-    digitized = np.digitize(confidence, bins)
+    digitized = np.digitize(confidence, bins[1:-1])
     
     w = np.array([(digitized==i).sum() for i in range(num_bins)])
     w = normalize(w, norm='l1')

spline.py
@@ -15,9 +15,9 @@ def spline_classification_plot(ax, X, y, X_eval, y_eval, gam_ref):
     gam = LogisticGAM(s(0, constraints='monotonic_inc')).gridsearch(X, y) # add a linear term
     #XX = gam.generate_X_grid(term=0)
     XX = np.linspace(0, 1, 100)
-    pdep, confi = gam.partial_dependence(term=0, width=.95)
+    pdep, confi_grid = gam.partial_dependence(term=0, width=.95)
     ax.plot(XX, sigmoid(pdep))
-    ax.plot(XX, sigmoid(confi), c='r', ls='--')
+    ax.plot(XX, sigmoid(confi_grid), c='r', ls='--')
     # compute ece and acc after calibration
     y_ = gam.predict_proba(X_eval)
     ece = EceEval(np.array([1-y_, y_]).T , y_eval, num_bins = 100)
@@ -30,6 +30,8 @@ def spline_classification_plot(ax, X, y, X_eval, y_eval, gam_ref):
     ax.set_ylim(0.0, 1.0)
 #     print gam.accuracy(X, y)
 #     print gam.summary()
+    # compute the confidence on datapoints of X_eval
+    confi = gam.confidence_intervals(X_eval, width=0.95)
     return ece, acc, mse, ax, confi
 
 def spline_classification(X, y, X_eval, y_eval, gam_ref):
@@ -37,25 +39,25 @@ def spline_classification(X, y, X_eval, y_eval, gam_ref):
     # documentation of LogisticGAM: https://pygam.readthedocs.io/en/latest/api/logisticgam.html
     gam = LogisticGAM(s(0, constraints='monotonic_inc')).gridsearch(X, y) # add a linear term
     #XX = gam.generate_X_grid(term=0)
-    XX = np.linspace(0, 1, 100)
-    pdep, confi = gam.partial_dependence(term=0, width=.95)
     # compute ece and acc after calibration
     y_ = gam.predict_proba(X_eval)
     ece = EceEval(np.array([1-y_, y_]).T , y_eval, num_bins = 100)
     mse = MseEval(gam, gam_ref, num_bins = 100)
     y_predict = y_ > 0.5
     acc = (y_predict == y_eval).mean()
+    # compute the confidence on datapoints of X_eval
+    confi = gam.confidence_intervals(X_eval, width=0.95)
     return ece, acc, mse, confi
 
-def get_spline_uncertainty(X, y):
-    # OUTPUT:
-    #     confi: dimensionality
-    # gam = LogisticGAM(s(0)).gridsearch(X, y)
-    # documentation of LogisticGAM: https://pygam.readthedocs.io/en/latest/api/logisticgam.html
-    gam = LogisticGAM(s(0, constraints='monotonic_inc')).gridsearch(X, y) # add a linear term
-    XX = np.linspace(0, 1, NUM_BINS+1)
-    pdep, confi = gam.partial_dependence(term=0, width=.95)
-    return confi
+# def get_spline_uncertainty(X, y):
+#     # OUTPUT:
+#     #     confi: dimensionality
+#     # gam = LogisticGAM(s(0)).gridsearch(X, y)
+#     # documentation of LogisticGAM: https://pygam.readthedocs.io/en/latest/api/logisticgam.html
+#     gam = LogisticGAM(s(0, constraints='monotonic_inc')).gridsearch(X, y) # add a linear term
+#     XX = np.linspace(0, 1, NUM_BINS+1)
+#     pdep, confi = gam.partial_dependence(term=0, width=.95)
+#     return confi
 
 def spline_calibration(X, y):
     gam = LogisticGAM(s(0, constraints='monotonic_inc')).gridsearch(X, y) # add a linear term

spline_run.py
@@ -23,12 +23,12 @@ NUM_SAMPLES = [20, 30, 40, 50, 60, 70, 80, 90, 100] + [50 * _ for _ in range(3, 
 print len(NUM_SAMPLES)
 
 
-DATASET = "cifar100_predictions_dropout"
-data = np.genfromtxt("data/cifar100/%s.txt" % DATASET)# 10000*101
+# DATASET = "cifar100_predictions_dropout"
+# data = np.genfromtxt("data/cifar100/%s.txt" % DATASET)# 10000*101
 # DATASET = "svhn_predictions"
 # data = np.genfromtxt("data/svhn/%s.txt" % DATASET)
-# DATASET = "cifar100_adversarial_predictions"
-# data = np.genfromtxt("data/cifar100_adversarial/%s.txt" % DATASET)# 10000*101
+DATASET = "cifar100_adversarial_predictions"
+data = np.genfromtxt("data/cifar100_adversarial/%s.txt" % DATASET)# 10000*101
 
 
 score = data[:,1:]

visualize.py
@@ -222,7 +222,7 @@ def reliability_plot(ax, p, y, num_bins=10):
     bins = np.linspace(0, 1, num_bins+1)
     
     confidence = np.max(p, axis=1)
-    digitized = np.digitize(confidence, bins)
+    digitized = np.digitize(confidence, bins[1:-1])
     
     w = np.array([(digitized==i).sum() for i in range(1, num_bins+1)])
     w = normalize(w, norm='l1')
@@ -256,7 +256,7 @@ def reliability_plot_binary(ax, p, y, num_bins=10):
     Y_predict = p > 0.5
     Y_true = y
     bins = np.linspace(0, 1, num_bins+1)
-    digitized = np.digitize(p, bins)
+    digitized = np.digitize(p, bins[1:-1])
     
     w = np.array([(digitized==i).sum() for i in range(1, num_bins+1)])
     w = normalize(w, norm='l1')
@@ -290,7 +290,7 @@ def reliability_plot_binary(ax, p, y, num_bins=10):
 # plot Brightness v.s. accuracy
 def brightness_plot_binary(ax, brightness, Y_predicted, Y_true, num_bins=10):
     bins = np.linspace(brightness.min(), brightness.max(), num_bins+1)
-    digitized = np.digitize(brightness, bins)
+    digitized = np.digitize(brightness, bins[1:-1])
     brightness_bins = np.array([brightness[digitized==i].mean() for i in range(1, num_bins+1)])
     accuracy_bins = np.array([(Y_true[digitized==i]==Y_predicted[digitized==i]).mean() for i in range(1, num_bins+1)])
     
@@ -337,6 +337,7 @@ def plot_metric_single_run(ece_random_emp, acc_random_emp, mse_random_emp,\
                  c = 'c', label="Random_Unf")
     ax[1].set_xlabel("#datapoints")
     ax[1].set_ylabel("Accuracy")
+    ax[1].legend()
     ax[2].plot(NUM_SAMPLES, 
                  [mse_active_prb[_] for _ in NUM_SAMPLES], 
                  c = 'r', label="Active_Prb")
@@ -351,6 +352,7 @@ def plot_metric_single_run(ece_random_emp, acc_random_emp, mse_random_emp,\
                  c = 'c', label="Random_Unf")
     ax[2].set_xlabel("#datapoints")
     ax[2].set_ylabel("MSE from GAM_ref")
+    ax[2].legend()
     plt.rc('legend',**{'fontsize':12})
     fig.tight_layout()
 